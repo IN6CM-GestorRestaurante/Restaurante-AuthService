460ce180111f5577f9f5d7cbcb8f0b75555f24ae
OlivMer765 <omerida-2021641@kinal.edu.gt>
2026-04-26 23:04:50 -0600

    feat: retornar detalles de usuario y roles en la respuesta json del login
    Se creo un nuevo objeto AuthResponseDto.cs, y se inyectaron sus propiedades modificando IAuthService.cs, AuthService.cs y el endpoint de Login dentro de AuthController.cs, esto debido a que la lógica del backend solamente retorna el String del JWT y un mensaje.

--- Restaurante.AuthService/Restaurante.AuthService.Application/Interfaces/IAuthService.cs
+++ Restaurante.AuthService/Restaurante.AuthService.Application/Interfaces/IAuthService.cs
@@ -4,8 +4,8 @@ namespace Restaurante.AuthService.Application.Interfaces;
 
 public interface IAuthService
 {
-    // Devuelve un token JWT (string) al hacer login
-    Task<string> LoginAsync(LoginDto loginDto);
+    // Devuelve un DTO con el token JWT y los datos del usuario al hacer login
+    Task<AuthResponseDto> LoginAsync(LoginDto loginDto);
     
     // Devuelve true/false o un DTO de respuesta al registrar
     Task<bool> RegisterAsync(RegisterDto registerDto);
