ce6b5c5f34b6e210a2844925329ed4dd2ea7e7d6
OlivMer765 <omerida-2021641@kinal.edu.gt>
2026-05-03 16:51:18 -0600

    feat: agregar seeder para admin - Se agregó seeder para administrador y configuración de contraseña en appsettings.json

---
+++ Restaurante.AuthService/Restaurante.AuthService.Infrastructure/Data/DataSeeder.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using Restaurante.AuthService.Domain.Entities;
+using Restaurante.AuthService.Application.Interfaces;
+using Microsoft.Extensions.Configuration;
+
+namespace Restaurante.AuthService.Infrastructure.Data
+{
+    public static class DataSeeder
+    {
+        public static async Task SeedAsync(AuthDbContext context, IPasswordHasher passwordHasher, IConfiguration configuration)
+        {
+            // Seed de un usuario administrador por defecto SOLO si no existen usuarios todavía
+            if (!await context.Users.AnyAsync())
+            {
+                var adminPassword = configuration["SeedSettings:AdminPassword"];
+                
+                if (string.IsNullOrEmpty(adminPassword))
+                {
+                    throw new ArgumentNullException("SeedSettings:AdminPassword", "La contraseña de administrador por defecto no está configurada.");
+                }
+
+                var adminUser = new User
+                {
+                    Email = "admin@restaurante.local",
+                    PasswordHash = passwordHasher.Hash(adminPassword),
+                    Role = "ADMIN",
+                    IsActive = true,
+                    CreatedAt = DateTime.UtcNow
+                };
+
+                await context.Users.AddAsync(adminUser);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
