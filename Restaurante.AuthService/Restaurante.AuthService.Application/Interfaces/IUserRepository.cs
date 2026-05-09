70b223ebcf819fe6fab5afa55ebee2f40dfcfb63
OlivMer765 <omerida-2021641@kinal.edu.gt>
2026-05-09 16:55:26 -0600

    feat: implementar entidad de dominio de usuario - Se implemento DbContext para el servicio de autenticación.

--- Restaurante.AuthService/Restaurante.AuthService.Application/Interfaces/IUserRepository.cs
+++ Restaurante.AuthService/Restaurante.AuthService.Application/Interfaces/IUserRepository.cs
@@ -4,7 +4,13 @@ namespace Restaurante.AuthService.Application.Interfaces;
 
 public interface IUserRepository
 {
+    Task<User?> GetByIdAsync(int id);
     Task<User?> GetByEmailAsync(string email);
+    Task<User?> GetByUsernameAsync(string username);
+    Task<User?> GetByEmailVerificationTokenAsync(string token);
+    Task<User?> GetByPasswordResetTokenAsync(string token);
+    Task<User?> GetByRefreshTokenAsync(string token);
     Task AddAsync(User user);
+    Task UpdateAsync(User user);
     Task SaveChangesAsync();
 }
