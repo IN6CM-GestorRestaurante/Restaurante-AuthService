c742ec7911d8fcaee75a279673738cda74f50ccb
ksolo-2022439 <ksolorzano-2022439@kinal.edu.gt>
2026-05-09 16:44:01 -0600

    feat: implementar servicio de autenticación principal con soporte para JWT - Servicio de autenticación y registro para usuarios y configuración de la base de datos.

--- Restaurante.AuthService/Restaurante.AuthService.Application/DTOs/RegisterDto.cs
+++ Restaurante.AuthService/Restaurante.AuthService.Application/DTOs/RegisterDto.cs
@@ -4,13 +4,17 @@ namespace Restaurante.AuthService.Application.DTOs;
 
     public class RegisterDto
     {
+        /// <summary>
+        /// Correo electrónico del usuario a registrar
+        /// </summary>
         [Required(ErrorMessage = "El email es obligatorio")]
         [EmailAddress(ErrorMessage = "Formato de email inválido")]
         public string Email { get; set; } = string.Empty;
 
+        /// <summary>
+        /// Contraseña del usuario
+        /// </summary>
         [Required(ErrorMessage = "La contraseña es obligatoria")]
         [MinLength(6, ErrorMessage = "La contraseña debe tener al menos 6 caracteres")]
         public string Password { get; set; } = string.Empty;
-
-        public string Role { get; set; } = "WAITER"; // Por defecto, asignamos rol de mesero
-    }
+    }
