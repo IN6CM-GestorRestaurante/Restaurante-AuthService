7e19267b1971fe51cca543f74eae6bf676deb6d4
SERV-DRV <sderosa-2023220@kinal.edu.gt>
2026-05-24 09:36:01 -0600

    refactor: actualizar mapeo de dominio y persistencia
    - Migración de entidad User y adaptación de configuraciones.
    - Refactorización de repositorio IUserRepository y UserRepository para ajustarse a Fluent API.

--- Restaurante.AuthService/Restaurante.AuthService.Application/Interfaces/IUserRepository.cs
+++ Restaurante.AuthService/Restaurante.AuthService.Application/Interfaces/IUserRepository.cs
@@ -8,6 +8,7 @@ public interface IUserRepository
     Task<User?> GetByEmailAsync(string email);
     Task<User?> GetByUsernameAsync(string username);
     Task<User?> GetByEmailVerificationTokenAsync(string token);
+    Task<User?> GetByVerificationOtpAsync(string otp);
     Task<User?> GetByPasswordResetTokenAsync(string token);
     Task<User?> GetByRefreshTokenAsync(string token);
     Task AddAsync(User user);
